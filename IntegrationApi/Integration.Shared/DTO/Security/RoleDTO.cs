d4793cc699589235c1b5d746bfec2a084b5f063e
epulido <erika1390@gmail.com>
2025-02-25 11:51:28 -0500

    RoleDTO

---
+++ IntegrationApi/Integration.Shared/DTO/Security/RoleDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Integration.Shared.DTO.Security
+{
+    public class RoleDTO
+    {
+        public int RoleId { get; set; }
+        public required string Code { get; set; }
+        public required string Name { get; set; }
+        public int ApplicationId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public required string CreatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
