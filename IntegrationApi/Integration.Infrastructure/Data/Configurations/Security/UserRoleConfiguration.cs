c9892ce6def9850bc554f865d4f97e17c07c895b
epulido <erika1390@gmail.com>
2025-03-06 12:35:34 -0500

    Update UserRoleConfiguration

--- IntegrationApi/Integration.Infrastructure/Data/Configurations/Security/UserRoleConfiguration.cs
+++ IntegrationApi/Integration.Infrastructure/Data/Configurations/Security/UserRoleConfiguration.cs
@@ -9,27 +9,16 @@ namespace Integration.Infrastructure.Data.Configurations.Security
         {
             builder.ToTable("UserRoles", "Security");
             builder.HasKey(e => new { e.UserId, e.RoleId});
-
-            builder.Property(e => e.CreatedAt)
-                .IsRequired();
-
+            builder.HasIndex(e => new { e.UserId, e.RoleId }).HasDatabaseName("IDX_UserRoles_UserId_RoleId");
+            builder.Property(e => e.CreatedAt).IsRequired();
             builder.Property(e => e.UpdatedAt);
-
-            builder.Property(e => e.CreatedBy)
-                .IsRequired()
-                .HasMaxLength(100);
-
-            builder.Property(e => e.UpdatedBy)
-                .HasMaxLength(100);
-
-            builder.Property(e => e.IsActive)
-                .IsRequired();
-
+            builder.Property(e => e.CreatedBy).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.UpdatedBy).HasMaxLength(100);
+            builder.Property(e => e.IsActive).IsRequired();
             builder.HasOne(e => e.User)
                 .WithMany(u => u.UserRoles)
                 .HasForeignKey(e => e.UserId)
                 .OnDelete(DeleteBehavior.Cascade);
-
             builder.HasOne(e => e.Role)
                 .WithMany(r => r.UserRoles)
                 .HasForeignKey(e => e.RoleId)
