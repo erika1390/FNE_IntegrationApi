8cb6bbb554d11b2778f57f0e4fb6497e9e58e1b9
epulido <erika1390@gmail.com>
2025-03-08 08:04:16 -0500

    LoggingMiddleware

--- IntegrationApi/Integration.Api/Middlewares/LoggingMiddleware.cs
+++ IntegrationApi/Integration.Api/Middlewares/LoggingMiddleware.cs
@@ -3,49 +3,46 @@ using System.Diagnostics;
 using System.Text;
 namespace Integration.Api.Middlewares
 {
-    public class LoggingMiddleware
+    public class C
     {
         private readonly RequestDelegate _next;
         private readonly ILogger<LoggingMiddleware> _logger;
-
         public LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger)
         {
             _next = next;
             _logger = logger;
         }
-
         public async Task Invoke(HttpContext context)
         {
             var stopwatch = Stopwatch.StartNew();
-
             context.Request.EnableBuffering();
             var requestBody = await new StreamReader(context.Request.Body, Encoding.UTF8, leaveOpen: true).ReadToEndAsync();
             context.Request.Body.Position = 0;
-
             var headers = JsonConvert.SerializeObject(context.Request.Headers.ToDictionary(h => h.Key, h => h.Value.ToString()));
-
             var originalBodyStream = context.Response.Body;
             using var responseBody = new MemoryStream();
             context.Response.Body = responseBody;
-
-            await _next(context);
-
-            stopwatch.Stop();
-
-            context.Response.Body.Seek(0, SeekOrigin.Begin);
-            var responseBodyText = await new StreamReader(context.Response.Body).ReadToEndAsync();
-            context.Response.Body.Seek(0, SeekOrigin.Begin);
-
-            _logger.LogInformation("HTTP {HttpMethod} {RequestPath} responded {StatusCode} in {ElapsedMilliseconds}ms",
-                context.Request.Method,
-                context.Request.Path,
-                context.Response.StatusCode,
-                stopwatch.ElapsedMilliseconds);
-
-            _logger.LogInformation("Request: {RequestBody}, Response: {ResponseBody}, Headers: {Headers}",
-                requestBody, responseBodyText, headers);
-
-            await responseBody.CopyToAsync(originalBodyStream);
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al procesar la solicitud");
+                throw;
+            }
+            finally
+            {
+                stopwatch.Stop();
+                context.Response.Body.Seek(0, SeekOrigin.Begin);
+                var responseBodyText = await new StreamReader(context.Response.Body).ReadToEndAsync();
+                context.Response.Body.Seek(0, SeekOrigin.Begin);
+                var logMessage = new StringBuilder();
+                logMessage.AppendLine($"HTTP {context.Request.Method} {context.Request.Path} responded {context.Response.StatusCode} in {stopwatch.ElapsedMilliseconds}ms");
+                logMessage.AppendLine($"Request: {requestBody}, Response: {responseBodyText}, Headers: {headers}");
+                _logger.LogInformation(logMessage.ToString());
+                await responseBody.CopyToAsync(originalBodyStream);
+            }
         }
     }
 }
