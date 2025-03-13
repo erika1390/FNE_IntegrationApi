6367a1c4cae898644267e7d8d35bacf9321f4445
epulido <erika1390@gmail.com>
2025-03-13 11:38:57 -0500

    pruebPruebas controladoresas controladores

--- IntegrationApi/Integration.Api.Test/Controllers/Security/ApplicationControllerTest.cs
+++ IntegrationApi/Integration.Api.Test/Controllers/Security/ApplicationControllerTest.cs
@@ -1,4 +1,12 @@
-﻿namespace Integration.Api.Tests.Controllers.Security
+﻿using Integration.Api.Controllers.Security;
+using Integration.Application.Interfaces.Security;
+using Integration.Shared.DTO.Security;
+using Integration.Shared.Response;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+namespace Integration.Api.Tests.Controllers.Security
 {
     [TestFixture]
     public class ApplicationControllerTest
--- IntegrationApi/Integration.Api.Test/Controllers/Security/AuthControllerTest.cs
+++ IntegrationApi/Integration.Api.Test/Controllers/Security/AuthControllerTest.cs
@@ -1,4 +1,17 @@
-﻿namespace Integration.Api.Tests.Controllers.Security
+﻿using Integration.Application.Exceptions;
+using Integration.Application.Interfaces.Security;
+using Integration.Shared.DTO.Security;
+using Integration.Shared.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using Integration.Api.Controllers.Security;
+
+namespace Integration.Api.Tests.Controllers.Security
 {
     [TestFixture]
     public class AuthControllerTest
