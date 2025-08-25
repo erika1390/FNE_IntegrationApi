ccabed6c19dbdd967e9db594b64bc28f008a491e
epulido <erika1390@gmail.com>
2025-08-25 11:15:36 -0500

    Ajuste rol

--- IntegrationApi/Integration.Application/Mappings/Security/RoleProfile.cs
+++ IntegrationApi/Integration.Application/Mappings/Security/RoleProfile.cs
@@ -10,6 +10,7 @@ namespace Integration.Application.Mappings.Security
             CreateMap<RoleDTO, Role>()
                 .ForMember(dest => dest.Code, opt => opt.MapFrom(src => src.Code))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.ApplicationId, opt => opt.MapFrom(src => src.ApplicationId))
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
                 .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => src.UpdatedAt))
                 .ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.CreatedBy))
